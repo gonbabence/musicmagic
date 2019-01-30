a712e1b7003289816bb4ca09ff711f6b42d23d33
Bence Balint <balint.bence@index.hu>
2019-01-30 22:24:28 +0100

    added contact form

--- resources/views/ads/details.blade.php
+++ resources/views/ads/details.blade.php
@@ -9,18 +9,14 @@
         <p><?=sprintf('%s - %s', $ad['mfr'], $ad['model'])?></p>
         <hr>
         <p><!-- image --></p>
-
         <hr>
-
         <p>Price: <?=sprintf('%s Ft', $ad['price'])?></p>
-
         <p class="active">Description:</p>
         <div class="col-md-12">
             <div class="tab-content">
                 {{ $ad['description'] }}
             </div>
         </div>
-
         <hr>
     </div>
 @endsection
