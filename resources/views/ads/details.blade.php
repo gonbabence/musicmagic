f230395819a01dbc10b70a4ff7f7090d51f804a4
Bence Balint <balint.bence@index.hu>
2019-03-05 21:40:35 +0100

    Reworked ad details page

--- resources/views/ads/details.blade.php
+++ resources/views/ads/details.blade.php
@@ -1,22 +1,29 @@
 @extends('layouts.default')
 @section('content')
-    {{--{{ $ad['mfr'] }}--}}
-    {{--{{ $ad['model'] }}--}}
-    {{--{{ $ad['description'] }}--}}
-    {{--{{ $ad['condition'] }}--}}
 
-    <div class="col-md-12">
-        <p>{{ $ad['mfr'] }} - {{ $ad['model'] }}</p>
-        <hr>
-        <p><!-- image --></p>
-        <hr>
-        <p>Price: {{ $ad['price'] }} Ft</p>
-        <p class="active">Description:</p>
-        <div class="col-md-12">
-            <div class="tab-content">
-                {{ $ad['description'] }}
+    <div class="container">
+        <div class="text-center">
+            <h3>{{ $ad['mfr'] }} - {{ $ad['model'] }}</h3>
+            <hr>
+
+            <div class="col-sm-8 offset-sm-2">
+                <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
             </div>
+
+            <hr>
+
+            <p>Price:</p>
+            <h4>{{ $ad['price'] }} Ft</h4>
+
+            <hr>
+            <p>Description:</p>
+
+            <div class="col-md-12">
+                <div class="tab-content">
+                    {{ $ad['description'] }}
+                </div>
+            </div>
+            <hr>
         </div>
-        <hr>
     </div>
 @endsection
