13453e9455820c2680397df16b0cabeeca62b684
Bence Balint <balint.bence@index.hu>
2019-03-05 22:48:57 +0100

    Package.json updates & template fixes

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,9 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>MusicShop</title>
 
-<!-- load bootstrap from a cdn -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
-<link href="/css/musicshop.css" rel="stylesheet">
+<link href="{!! asset('css/app.css') !!}" rel="stylesheet">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="/js/musicshop.js"></script>
+<script src="{!! asset('js/app.js') !!}"></script>
